8e3b88b97eaa1f3fdfeae8c4b003d110a2ce9ea6
Saftophobia <asafty2003@gmail.com>
2014-08-27 21:21:03 +0300

    nav bar and routes modified

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
-  def home
-  end
+	before_action :authenticate_user!
+
+	def home
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,20 @@ Rails.application.routes.draw do
   get 'pages/home'
 
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'pages#home', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'pages#home'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
