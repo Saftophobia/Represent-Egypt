44481ed08176409e753d969e638fdbfbe1d7c503
Saftophobia <asafty2003@gmail.com>
2014-09-02 16:54:38 +0300

    added multiselect gem to project + multiselect menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require bootstrap-multiselect
 //= require turbolinks
 //= require underscore
 //= require gmaps/google
--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -32,7 +32,7 @@ class EntitiesController < ApplicationController
       @entity = Entity.new(entity_params)
       @entity.user_id = current_user.id
       @entity.admin_verification = false
-
+      
       respond_to do |format|
         if @entity.save
           format.html { redirect_to @entity, notice: 'Entity was successfully created.' }
@@ -50,7 +50,7 @@ class EntitiesController < ApplicationController
   def update
     respond_to do |format|
       if @entity.update(entity_params)
-
+          p "********************************************** "+@entity.type.to_s
         unless current_user.try(:admin)
           @entity.user_id = current_user.id
           @entity.admin_verification = false
@@ -86,9 +86,15 @@ class EntitiesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def entity_params
       if current_user.try(:admin)
-        params.require(:entity).permit(:name, :type, :lat, :lon, :description, :url, :address, :year_estab, :user_id, :admin_verification)
+        params.require(:entity).permit(:name, {:type => []}, :lat, :lon, :description, :url, :address, :year_estab, :user_id, :admin_verification)
       else
-        params.require(:entity).permit(:name, :type, :lat, :lon, :description, :url, :address, :year_estab)
+        params.require(:entity).permit(:name, {:type => []}, :lat, :lon, :description, :url, :address, :year_estab)
       end
     end
 end
+
+
+
+# [\"\"]" 
+# [\"\", \"Startup\"]"
+# [\"\", \"Startup\", \"Accelerator\", \"Co-worker\", \"R&D Center\"]"
